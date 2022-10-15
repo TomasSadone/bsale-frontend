1e5303057a391cbb2097f06ed23542133e56340d
TomasSadone <tomisadone27@gmail.com>
2022-10-14 22:59:25 -0300

    added dynamic categories

--- index.js
+++ index.js
@@ -12,3 +12,41 @@ hamburguerMenu.addEventListener("click", () => {
     hamburguerMenu.setAttribute("data-visible", false);
   }
 });
+
+let categories;
+let selectedCategory;
+
+/*  -- GET CATEGORIES --  */
+
+(async () => {
+  const url = "http://localhost:3500/categories";
+  try {
+    const resp = await fetch(url, { method: "GET" });
+    const data = await resp.json();
+    console.log(data);
+    categories = data;
+    selectedCategory = categories[0];
+    appendCategories(categories);
+    /*  -- GET PRODUCTS FROM INITIAL CATEGORY --  */
+    getCategoryProducts(selectedCategory);
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
+const getCategoryProducts = async category => {
+  const url = `http://localhost:3500/category/${category.id}`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {}
+};
+
+const appendCategories = categories => {
+  categories.forEach(category => {
+    const li = document.createElement("li");
+    li.innerHTML = category.name;
+    primaryNav.append(li);
+  });
+};
